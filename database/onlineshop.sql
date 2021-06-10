c035d93ff195b66d17e2b38d0ce15abdcdfe639c
nguyentruongnvt72 <quantruong1999>
2021-06-10 09:40:26 +0700

    db

--- database/onlineshop.sql
+++ database/onlineshop.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2019 at 07:53 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Jun 10, 2021 at 04:29 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ecommerece`
+-- Database: `onlineshop`
 --
 
 DELIMITER $$
@@ -66,12 +65,12 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
-(1, 'HP'),
-(2, 'Samsung'),
-(3, 'Apple'),
-(4, 'motorolla'),
-(5, 'LG'),
-(6, 'Cloth Brand');
+(1, 'Hoa Hong'),
+(2, 'Hoa Lan'),
+(3, 'Hoa Cuc'),
+(4, 'Hoa Tulip'),
+(5, 'Hoa Sen'),
+(6, 'Hoa Ly');
 
 -- --------------------------------------------------------
 
@@ -108,7 +107,20 @@ INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`) VALUES
 (55, 5, '::1', 14, 1),
 (56, 1, '::1', 9, 1),
 (57, 2, '::1', 9, 1),
-(71, 61, '127.0.0.1', -1, 1);
+(71, 61, '127.0.0.1', -1, 1),
+(150, 12, '14.231.195.203', 26, 1),
+(151, 33, '113.190.222.131', 27, 1),
+(152, 34, '113.190.222.131', 27, 1),
+(153, 32, '::1', -1, 1),
+(156, 79, '123.24.170.185', 26, 1),
+(157, 82, '125.212.158.161', 29, 1),
+(158, 83, '125.212.158.161', 29, 1),
+(160, 83, '14.231.195.203', 26, 1),
+(161, 84, '14.231.195.203', 26, 1),
+(163, 90, '113.186.158.225', -1, 1),
+(164, 83, '113.186.158.225', -1, 1),
+(165, 83, '123.24.170.185', -1, 1),
+(166, 98, '14.177.154.66', -1, 1);
 
 -- --------------------------------------------------------
 
@@ -118,7 +130,7 @@ INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`) VALUES
 
 CREATE TABLE `categories` (
   `cat_id` int(100) NOT NULL,
-  `cat_title` text NOT NULL
+  `cat_title` text CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -126,13 +138,13 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
-(1, 'Electronics'),
-(2, 'Ladies Wears'),
-(3, 'Mens Wear'),
-(4, 'Kids Wear'),
-(5, 'Furnitures'),
-(6, 'Home Appliances'),
-(7, 'Electronics Gadgets');
+(1, 'Hoa Sinh Nhat'),
+(2, 'Hoa Chuc Mung'),
+(3, 'Hoa Chia Buon'),
+(4, 'Hoa Khai Truong'),
+(5, 'Hoa Lang Man'),
+(6, 'Hoa Qua Tang'),
+(7, 'Hoa Bo');
 
 -- --------------------------------------------------------
 
@@ -218,7 +230,11 @@ CREATE TABLE `orders_info` (
 --
 
 INSERT INTO `orders_info` (`order_id`, `user_id`, `f_name`, `email`, `address`, `city`, `state`, `zip`, `cardname`, `cardnumber`, `expdate`, `prod_count`, `total_amt`, `cvv`) VALUES
-(1, 12, 'Puneeth', 'puneethreddy951@gmail.com', 'Bangalore, Kumbalagodu, Karnataka', 'Bangalore', 'Karnataka', 560074, 'pokjhgfcxc', '4321 2345 6788 7654', '12/90', 3, 77000, 1234);
+(1, 12, 'Puneeth', 'puneethreddy951@gmail.com', 'Bangalore, Kumbalagodu, Karnataka', 'Bangalore', 'Karnataka', 560074, 'pokjhgfcxc', '4321 2345 6788 7654', '12/90', 3, 77000, 1234),
+(2, 1, ' truong', 'lethu3454lt@gmail.com', 'edfd', 'adf', 'tryu', 100000, 'dfsdfas', '24324325423', '09/12', 1, 5000, 2342),
+(3, 1, 'truong', 'lethu3454lt@gmail.com', 'edfd', 'adf', 'tryu', 100000, 'ngyeutruong', '5345 345', '12/23', 1, 399, 1231),
+(4, 12, 'puneeth Reddy', 'puneethreddy951@gmail.com', 'dsfsf, a', 'kk', 'd', 100000, 'dd', '4242 4242 4242 4242', '02/02', 1, 2500, 333),
+(5, 28, 'nguyen cong van', 'nguyentruong@gmail.com', 'ha', 'hs', 'dsfdfds', 100000, 'xgdsfgdsgdfsgdsf', '23424324', '12/12', 1, 399, 213);
 
 -- --------------------------------------------------------
 
@@ -241,7 +257,11 @@ CREATE TABLE `order_products` (
 INSERT INTO `order_products` (`order_pro_id`, `order_id`, `product_id`, `qty`, `amt`) VALUES
 (73, 1, 1, 1, 5000),
 (74, 1, 4, 2, 64000),
-(75, 1, 8, 1, 40000);
+(75, 1, 8, 1, 40000),
+(91, 2, 71, 1, 5000),
+(92, 3, 63, 1, 399),
+(93, 4, 32, 1, 2500),
+(94, 5, 66, 1, 399);
 
 -- --------------------------------------------------------
 
@@ -253,9 +273,9 @@ CREATE TABLE `products` (
   `product_id` int(100) NOT NULL,
   `product_cat` int(100) NOT NULL,
   `product_brand` int(100) NOT NULL,
-  `product_title` varchar(255) NOT NULL,
+  `product_title` varchar(255) CHARACTER SET utf8 NOT NULL,
   `product_price` int(100) NOT NULL,
-  `product_desc` text NOT NULL,
+  `product_desc` text CHARACTER SET utf8 NOT NULL,
   `product_image` text NOT NULL,
   `product_keywords` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -265,77 +285,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`) VALUES
-(1, 1, 2, 'Samsung galaxy s7 edge', 5000, 'Samsung galaxy s7 edge', 'product07.png', 'samsung mobile electronics'),
-(2, 1, 3, 'iPhone 5s', 25000, 'iphone 5s', 'http___pluspng.com_img-png_iphone-hd-png-iphone-apple-png-file-550.png', 'mobile iphone apple'),
-(3, 1, 3, 'iPad air 2', 30000, 'ipad apple brand', 'da4371ffa192a115f922b1c0dff88193.png', 'apple ipad tablet'),
-(4, 1, 3, 'iPhone 6s', 32000, 'Apple iPhone ', 'http___pluspng.com_img-png_iphone-6s-png-iphone-6s-gold-64gb-1000.png', 'iphone apple mobile'),
-(5, 1, 2, 'iPad 2', 10000, 'samsung ipad', 'iPad-air.png', 'ipad tablet samsung'),
-(6, 1, 1, 'samsung Laptop r series', 35000, 'samsung Black combination Laptop', 'laptop_PNG5939.png', 'samsung laptop '),
-(7, 1, 1, 'Laptop Pavillion', 50000, 'Laptop Hp Pavillion', 'laptop_PNG5930.png', 'Laptop Hp Pavillion'),
-(8, 1, 4, 'Sony', 40000, 'Sony Mobile', '530201353846AM_635_sony_xperia_z.png', 'sony mobile'),
-(9, 1, 3, 'iPhone New', 12000, 'iphone', 'iphone-hd-png-iphone-apple-png-file-550.png', 'iphone apple mobile'),
-(10, 2, 6, 'Red Ladies dress', 1000, 'red dress for girls', 'red dress.jpg', 'red dress '),
-(11, 2, 6, 'Blue Heave dress', 1200, 'Blue dress', 'images.jpg', 'blue dress cloths'),
-(12, 2, 6, 'Ladies Casual Cloths', 1500, 'ladies casual summer two colors pleted', '7475-ladies-casual-dresses-summer-two-colors-pleated.jpg', 'girl dress cloths casual'),
-(13, 2, 6, 'SpringAutumnDress', 1200, 'girls dress', 'Spring-Autumn-Winter-Young-Ladies-Casual-Wool-Dress-Women-s-One-Piece-Dresse-Dating-Clothes-Medium.jpg_640x640.jpg', 'girl dress'),
-(14, 2, 6, 'Casual Dress', 1400, 'girl dress', 'download.jpg', 'ladies cloths girl'),
-(15, 2, 6, 'Formal Look', 1500, 'girl dress', 'shutterstock_203611819.jpg', 'ladies wears dress girl'),
-(16, 3, 6, 'Sweter for men', 600, '2012-Winter-Sweater-for-Men-for-better-outlook', '2012-Winter-Sweater-for-Men-for-better-outlook.jpg', 'black sweter cloth winter'),
-(17, 3, 6, 'Gents formal', 1000, 'gents formal look', 'gents-formal-250x250.jpg', 'gents wear cloths'),
-(19, 3, 6, 'Formal Coat', 3000, 'ad', 'images (1).jpg', 'coat blazer gents'),
-(20, 3, 6, 'Mens Sweeter', 1600, 'jg', 'Winter-fashion-men-burst-sweater.png', 'sweeter gents '),
-(21, 3, 6, 'T shirt', 800, 'ssds', 'IN-Mens-Apparel-Voodoo-Tiles-09._V333872612_.jpg', 'formal t shirt black'),
-(22, 4, 6, 'Yellow T shirt ', 1300, 'yello t shirt with pant', '1.0x0.jpg', 'kids yellow t shirt'),
-(23, 4, 6, 'Girls cloths', 1900, 'sadsf', 'GirlsClothing_Widgets.jpg', 'formal kids wear dress'),
-(24, 4, 6, 'Blue T shirt', 700, 'g', 'images.jpg', 'kids dress'),
-(25, 4, 6, 'Yellow girls dress', 750, 'as', 'images (3).jpg', 'yellow kids dress'),
-(27, 4, 6, 'Formal look', 690, 'sd', 'image28.jpg', 'formal kids dress'),
-(32, 5, 0, 'Book Shelf', 2500, 'book shelf', 'furniture-book-shelf-250x250.jpg', 'book shelf furniture'),
-(33, 6, 2, 'Refrigerator', 35000, 'Refrigerator', 'CT_WM_BTS-BTC-AppliancesHome_20150723.jpg', 'refrigerator samsung'),
-(34, 6, 4, 'Emergency Light', 1000, 'Emergency Light', 'emergency light.JPG', 'emergency light'),
-(35, 6, 0, 'Vaccum Cleaner', 6000, 'Vaccum Cleaner', 'images (2).jpg', 'Vaccum Cleaner'),
-(36, 6, 5, 'Iron', 1500, 'gj', 'iron.JPG', 'iron'),
-(37, 6, 5, 'LED TV', 20000, 'LED TV', 'images (4).jpg', 'led tv lg'),
-(38, 6, 4, 'Microwave Oven', 3500, 'Microwave Oven', 'images.jpg', 'Microwave Oven'),
-(39, 6, 5, 'Mixer Grinder', 2500, 'Mixer Grinder', 'singer-mixer-grinder-mg-46-medium_4bfa018096c25dec7ba0af40662856ef.jpg', 'Mixer Grinder'),
-(40, 2, 6, 'Formal girls dress', 3000, 'Formal girls dress', 'girl-walking.jpg', 'ladies'),
-(45, 1, 2, 'Samsung Galaxy Note 3', 10000, '0', 'samsung_galaxy_note3_note3neo.JPG', 'samsung galaxy Note 3 neo'),
-(46, 1, 2, 'Samsung Galaxy Note 3', 10000, '', 'samsung_galaxy_note3_note3neo.JPG', 'samsung galxaxy note 3 neo'),
-(47, 4, 6, 'Laptop', 650, 'nbk', 'product01.png', 'Dell Laptop'),
-(48, 1, 7, 'Headphones', 250, 'Headphones', 'product05.png', 'Headphones Sony'),
-(49, 1, 7, 'Headphones', 250, 'Headphones', 'product05.png', 'Headphones Sony'),
-(50, 3, 6, 'boys shirts', 350, 'shirts', 'pm1.JPG', 'suit boys shirts'),
-(51, 3, 6, 'boys shirts', 270, 'shirts', 'pm2.JPG', 'suit boys shirts'),
-(52, 3, 6, 'boys shirts', 453, 'shirts', 'pm3.JPG', 'suit boys shirts'),
-(53, 3, 6, 'boys shirts', 220, 'shirts', 'ms1.JPG', 'suit boys shirts'),
-(54, 3, 6, 'boys shirts', 290, 'shirts', 'ms2.JPG', 'suit boys shirts'),
-(55, 3, 6, 'boys shirts', 259, 'shirts', 'ms3.JPG', 'suit boys shirts'),
-(56, 3, 6, 'boys shirts', 299, 'shirts', 'pm7.JPG', 'suit boys shirts'),
-(57, 3, 6, 'boys shirts', 260, 'shirts', 'i3.JPG', 'suit boys shirts'),
-(58, 3, 6, 'boys shirts', 350, 'shirts', 'pm9.JPG', 'suit boys shirts'),
-(59, 3, 6, 'boys shirts', 855, 'shirts', 'a2.JPG', 'suit boys shirts'),
-(60, 3, 6, 'boys shirts', 150, 'shirts', 'pm11.JPG', 'suit boys shirts'),
-(61, 3, 6, 'boys shirts', 215, 'shirts', 'pm12.JPG', 'suit boys shirts'),
-(62, 3, 6, 'boys shirts', 299, 'shirts', 'pm13.JPG', 'suit boys shirts'),
-(63, 3, 6, 'boys Jeans Pant', 550, 'Pants', 'pt1.JPG', 'boys Jeans Pant'),
-(64, 3, 6, 'boys Jeans Pant', 460, 'pants', 'pt2.JPG', 'boys Jeans Pant'),
-(65, 3, 6, 'boys Jeans Pant', 470, 'pants', 'pt3.JPG', 'boys Jeans Pant'),
-(66, 3, 6, 'boys Jeans Pant', 480, 'pants', 'pt4.JPG', 'boys Jeans Pant'),
-(67, 3, 6, 'boys Jeans Pant', 360, 'pants', 'pt5.JPG', 'boys Jeans Pant'),
-(68, 3, 6, 'boys Jeans Pant', 550, 'pants', 'pt6.JPG', 'boys Jeans Pant'),
-(69, 3, 6, 'boys Jeans Pant', 390, 'pants', 'pt7.JPG', 'boys Jeans Pant'),
-(70, 3, 6, 'boys Jeans Pant', 399, 'pants', 'pt8.JPG', 'boys Jeans Pant'),
-(71, 1, 2, 'Samsung galaxy s7', 5000, 'Samsung galaxy s7', 'product07.png', 'samsung mobile electronics'),
-(72, 7, 2, 'sony Headphones', 3500, 'sony Headphones', 'product02.png', 'sony Headphones electronics gadgets'),
-(73, 7, 2, 'samsung Headphones', 3500, 'samsung Headphones', 'product05.png', 'samsung Headphones electronics gadgets'),
-(74, 1, 1, 'HP i5 laptop', 5500, 'HP i5 laptop', 'product01.png', 'HP i5 laptop electronics'),
-(75, 1, 1, 'HP i7 laptop 8gb ram', 5500, 'HP i7 laptop 8gb ram', 'product03.png', 'HP i7 laptop 8gb ram electronics'),
-(76, 1, 5, 'sony note 6gb ram', 4500, 'sony note 6gb ram', 'product04.png', 'sony note 6gb ram mobile electronics'),
-(77, 1, 4, 'MSV laptop 16gb ram NVIDEA Graphics', 5499, 'MSV laptop 16gb ram', 'product06.png', 'MSV laptop 16gb ram NVIDEA Graphics electronics'),
-(78, 1, 5, 'dell laptop 8gb ram intel integerated Graphics', 4579, 'dell laptop 8gb ram intel integerated Graphics', 'product08.png', 'dell laptop 8gb ram intel integerated Graphics electronics'),
-(79, 7, 2, 'camera with 3D pixels', 2569, 'camera with 3D pixels', 'product09.png', 'camera with 3D pixels camera electronics gadgets'),
-(80, 1, 1, 'ytrfdkjsd', 12343, 'sdfhgh', '1542455446_thythtf .jpeg', 'dfgh'),
-(81, 4, 6, 'Kids blue dress', 300, 'blue dress', '1543993724_pg4.jpg', 'kids blue dress');
+(95, 1, 1, 'Fall For You', 170000, 'Miễn Phí Vận Chuyển cho hóa đơn trên 199,000đ tại khu vực nội thành Tp.HCM & Hà Nội.\r\nGiao hàng trong ngày tại khu vực Tp.HCM & Hà Nội khi đặt hàng trước 17h mỗi ngày; và giao hàng từ 2-3 ngày cho các tỉnh/ thành khác toàn quốc. \r\n\r\nBó hoa Hồng đỏ đầy lãng mạn là món quà hoàn hảo thay lời muốn nói gửi đến người thương cũa bạn vào Valentine hoặc ngày kỷ niệm, sinh nhật.\r\nBó Hoa Hồng Fall For You gồm:\r\n\r\n- 24 cành Hoa Hồng\r\n\r\n- Các loại Hoa và Lá khác\r\n\r\n *Do mỗi sản phẩm đều được làm thủ công nên sẽ có chút khác biệt so với hình ảnh sẵn có trên website.', '1622382158_ApjKg7LVkLk6V0aWAbzGfzCOWfZ66JNvDkuNnAmL.jpeg', ''),
+(96, 5, 1, 'Fabulously Red', 160000, 'Miễn Phí Vận Chuyển cho hóa đơn trên 199,000đ tại khu vực nội thành Tp.HCM & Hà Nội.\r\nGiao hàng trong ngày tại khu vực Tp.HCM & Hà Nội khi đặt hàng trước 17h mỗi ngày; và giao hàng từ 2-3 ngày cho các tỉnh/ thành khác toàn quốc. \r\n\r\n---------\r\nBó hoa sang trọng và thanh khiết với hoa Hồng đỏ được gói xinh xắn.\r\nĐây sẽ là món quà bất ngờ và hoàn hảo dành tặng người thương, gia đình hoặc bạn bè. \r\nBó Hoa Fabulously Red gồm:\r\n\r\n- 12 bông Hoa Hồng\r\n\r\n- 06 bông Cẩm Chướng\r\n\r\n- Các loại hoa và lá khác\r\n\r\n* Do mỗi sản phẩm đều được làm thủ công nên sẽ có chút khác biệt so với hình ảnh sẵn có trên website.', '1622600945_ApjKg7LVkLk6V0aWAbzGfzCOWfZ66JNvDkuNnAmL.jpeg', ''),
+(97, 2, 2, 'Mãnh liệt', 170000, 'Sản phẩm bao gồm:\r\n\r\n+ Hồng vàng chùa\r\n\r\n+ Hồng kem hồng\r\n\r\n+ Hoa baby\r\n\r\n+ Lá tùng gai\r\n\r\n– Miễn phí ship toàn Thành Phố.\r\n\r\n– Miễn phí hóa đơn đỏ nếu quý khách có nhu cầu.\r\n\r\n– Miễn phí băng rôn hoặc thiệp chúc mừng.\r\n\r\n– Cam kết hài lòng quý khách', '1622601009_5daef25ddc54290a7045.jpeg', ''),
+(98, 4, 2, 'Mừng sinh nhật 96', 100000, 'Mừng sinh nhật 96', '1622601101_cc6a98ab3033ce6d97228-e1608990577937.jpeg', ''),
+(99, 3, 3, 'Mừng Sinh Nhật 28', 170000, 'Mừng Sinh Nhật 28', '1622601174_91a6f76bac2953770a38-1.jpeg', '');
 
 -- --------------------------------------------------------
 
@@ -367,7 +321,11 @@ INSERT INTO `user_info` (`user_id`, `first_name`, `last_name`, `email`, `passwor
 (22, 'puneeth', 'v', 'hemu@gmail.com', '1234534', '9877654334', 'snhdgvajfehyfygv', 'asdjbhfkeur'),
 (23, 'hemanth', 'reddy', 'hemanth@gmail.com', 'Puneeth@123', '9876543234', 'Bangalore', 'Kumbalagodu'),
 (24, 'newuser', 'user', 'newuser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu'),
-(25, 'otheruser', 'user', 'otheruser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu');
+(25, 'otheruser', 'user', 'otheruser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu'),
+(26, 'cuong', 'nguyen', 'nguyencuong@gmail.com', '123456789', '1111111111', 'ha', 'ha'),
+(27, 'manh', 'manh', 'tavanmanh1111@gmail.com', 'manh1234#', '0326648389', 'Thôn Bình l?ng xã Ti?n PHong Huy?n Ân thi', 'ssd'),
+(28, 'nguyen cong', 'van', 'nguyentruong@gmail.com', '123456789', '1111111111', 'ha', 'hs'),
+(29, 'Nguyen', 'Trang', 'tranga1k17@gmail.com', 'trang1999', '0123456789', 'Ngh? An', 'Tym');
 
 --
 -- Triggers `user_info`
@@ -411,7 +369,11 @@ INSERT INTO `user_info_backup` (`user_id`, `first_name`, `last_name`, `email`, `
 (22, 'puneeth', 'v', 'hemu@gmail.com', '1234534', '9877654334', 'snhdgvajfehyfygv', 'asdjbhfkeur'),
 (23, 'hemanth', 'reddy', 'hemanth@gmail.com', 'Puneeth@123', '9876543234', 'Bangalore', 'Kumbalagodu'),
 (24, 'newuser', 'user', 'newuser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu'),
-(25, 'otheruser', 'user', 'otheruser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu');
+(25, 'otheruser', 'user', 'otheruser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu'),
+(26, 'cuong', 'nguyen', 'nguyencuong@gmail.com', '123456789', '1111111111', 'ha', 'ha'),
+(27, 'manh', 'manh', 'tavanmanh1111@gmail.com', 'manh1234#', '0326648389', 'Thôn Bình l?ng xã Ti?n PHong Huy?n Ân thi', 'ssd'),
+(28, 'nguyen cong', 'van', 'nguyentruong@gmail.com', '123456789', '1111111111', 'ha', 'hs'),
+(29, 'Nguyen', 'Trang', 'tranga1k17@gmail.com', 'trang1999', '0123456789', 'Ngh? An', 'Tym');
 
 --
 -- Indexes for dumped tables
@@ -512,7 +474,7 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=167;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -548,42 +510,25 @@ ALTER TABLE `orders_info`
 -- AUTO_INCREMENT for table `order_products`
 --
 ALTER TABLE `order_products`
-  MODIFY `order_pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `order_pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT for table `user_info`
 --
 ALTER TABLE `user_info`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `user_info_backup`
 --
 ALTER TABLE `user_info_backup`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders_info`
---
-ALTER TABLE `orders_info`
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
-
---
--- Constraints for table `order_products`
---
-ALTER TABLE `order_products`
-  ADD CONSTRAINT `order_products` FOREIGN KEY (`order_id`) REFERENCES `orders_info` (`order_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
